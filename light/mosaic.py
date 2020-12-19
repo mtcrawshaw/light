c5c459d724ea5f8b4ff7f211d95f005dad472fe0
Michael Crawshaw <michael.crawshaw.4@gmail.com>
2020-12-18 22:49:49 -0500

    Added notes for mosaic generation and started mosaic implementation.

---
+++ light/mosaic.py
@@ -0,0 +1,7 @@
+from typing import Dict, Any
+
+
+def create_mosaic(config: Dict[str, Any]) -> None:
+    """ Create a gradient-colored mosaic from an image. """
+
+    print(config)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import argparse
 import json
 
 from light.gan import train_gan
+from light.mosaic import create_mosaic
 
 
 def main(args: argparse.Namespace):
@@ -16,13 +17,15 @@ def main(args: argparse.Namespace):
     # Call appropriate command.
     if args.command == "gan":
         train_gan(config)
+    elif args.command == "mosaic":
+        create_mosaic(config)
     else:
         raise NotImplementedError
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("command", type=str, help="What to run. Options: 'gan'.")
+    parser.add_argument("command", type=str, help="What to run. Options: ['gan', 'mosaic'].")
     parser.add_argument("config_path", type=str, help="Path to config file.")
     args = parser.parse_args()
     main(args)

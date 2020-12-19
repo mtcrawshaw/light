63ea5089ff0e8b5e3950c1588771569830b015fb
Michael Crawshaw <michael.crawshaw.4@gmail.com>
2020-12-19 02:36:52 -0500

    Implemented runner for mosaic command.

--- light/mosaic/__init__.py
+++ light/mosaic/__init__.py
@@ -2,3 +2,4 @@ from light.mosaic.shape import Shape
 from light.mosaic.circle import Circle
 from light.mosaic.triangle import Triangle
 from light.mosaic.square import Square
+from light.mosaic.canvas import Canvas
--- light/mosaic/mosaic.py
+++ light/mosaic/mosaic.py
@@ -1,10 +1,59 @@
 """ Create a gradient-colored mosaic from an image. """
 
-
+import os
+import random
+import pickle
+import json
 from typing import Dict, Any
+
+from PIL import Image
+
+from light.mosaic import Canvas
 
 
 def create_mosaic(config: Dict[str, Any]) -> None:
     """ Create a gradient-colored mosaic from an image. """
 
-    print(config)
+    # Check uniqueness of save name.
+    save_dir = None
+    if config["save_name"] is not None:
+        save_dir = os.path.join("results", config["save_name"])
+        if os.path.isdir(save_dir):
+            print(
+                "Results directory '%s' already exists! Save names must be unique."
+                % save_dir
+            )
+        else:
+            os.makedirs(save_dir)
+
+    # Set random seed.
+    random.seed(config["seed"])
+
+    # Read in image, instantiate canvas, partition and color canvas.
+    image = Image.open(config["image_path"])
+    canvas = Canvas(image, config["num_splits"], config["max_child_area"], config["num_samples"])
+    # canvas.partition()
+    mosaic = canvas.color()
+
+    # Save out results.
+    if save_dir is not None:
+
+        # Save config.
+        config_path = os.path.join(save_dir, "%s_config.json" % config["save_name"])
+        with open(config_path, "w") as config_file:
+            json.dump(config, config_file, indent=4)
+
+        # Save original image.
+        dot_pos = config["image_path"].rfind(".")
+        image_ext = config["image_path"][dot_pos + 1:]
+        image_path = os.path.join(save_dir, "%s_image.%s" % (config["save_name"], image_ext))
+        image.save(image_path)
+
+        # Save canvas.
+        canvas_path = os.path.join(save_dir, "%s_canvas.pkl" % config["save_name"])
+        with open(canvas_path, "wb") as canvas_file:
+            pickle.dump(canvas, canvas_file)
+
+        # Save mosaic.
+        mosaic_path = os.path.join(save_dir, "%s_mosaic.%s" % (config["save_name"], image_ext))
+        mosaic.save(mosaic_path)
